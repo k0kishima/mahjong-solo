928d7eef789fc975e4948b6ac92a7a61105f2cc6
Johndoe <56298669+togane@users.noreply.github.com>
2022-07-12 20:57:06 +0900

    scaffold layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Hand } from '@/components';
+import { Tile } from '@/types';
 
-import logo from '@/logo.svg';
-import '@/App.css';
+const tiles: Tile[] = [
+  { tileType: 'MAN', tileNumber: 2 },
+  { tileType: 'MAN', tileNumber: 3 },
+  { tileType: 'MAN', tileNumber: 6 },
+  { tileType: 'MAN', tileNumber: 7 },
+  { tileType: 'MAN', tileNumber: 7 },
+  { tileType: 'PIN', tileNumber: 2 },
+  { tileType: 'PIN', tileNumber: 3 },
+  { tileType: 'PIN', tileNumber: 9 },
+  { tileType: 'PIN', tileNumber: 9 },
+  { tileType: 'SOU', tileNumber: 2 },
+  { tileType: 'SOU', tileNumber: 3 },
+  { tileType: 'SOU', tileNumber: 4 },
+  { tileType: 'SOU', tileNumber: 5 },
+  { tileType: 'SOU', tileNumber: 6 },
+];
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Styled>
+      <div className='main'></div>
+      <div className='hand'>
+        <Hand tiles={tiles} />
+      </div>
+    </Styled>
   );
 }
 
 export default App;
+
+const Styled = styled.div`
+  margin: 0px;
+  padding: 0px;
+  height: 100vh;
+  background-color: green;
+
+  @media screen and (orientation: portrait) {
+    .main {
+      height: 93vh;
+    }
+    .hand {
+      height: 7vh;
+    }
+  }
+
+  @media screen and (orientation: landscape) {
+    .main {
+      height: 90vh;
+    }
+    .hand {
+      height: 10vh;
+    }
+  }
+`;
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { Hand } from './Hand';
+export { Tile } from './Tile';
