e6a73f1226cbf97e1af3d54b49a1676de0d1a77a
Johndoe <56298669+togane@users.noreply.github.com>
2022-07-13 20:18:55 +0900

    reorganize components

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Hand } from '@/components';
-import { Tile } from '@/types';
-
-const tiles: Tile[] = [
-  { tileColor: 'MAN', tileNumber: 2 },
-  { tileColor: 'MAN', tileNumber: 3 },
-  { tileColor: 'MAN', tileNumber: 6 },
-  { tileColor: 'MAN', tileNumber: 7 },
-  { tileColor: 'MAN', tileNumber: 7 },
-  { tileColor: 'PIN', tileNumber: 2 },
-  { tileColor: 'PIN', tileNumber: 3 },
-  { tileColor: 'PIN', tileNumber: 9 },
-  { tileColor: 'PIN', tileNumber: 9 },
-  { tileColor: 'SOU', tileNumber: 2 },
-  { tileColor: 'SOU', tileNumber: 3 },
-  { tileColor: 'SOU', tileNumber: 4 },
-  { tileColor: 'SOU', tileNumber: 5 },
-  { tileColor: 'SOU', tileNumber: 6 },
-];
+import { Game } from '@/features/game';
 
 function App() {
-  return (
-    <Styled>
-      <div className='main'></div>
-      <div className='hand'>
-        <Hand tiles={tiles} />
-      </div>
-    </Styled>
-  );
+  return <Game />;
 }
 
 export default App;
-
-const Styled = styled.div`
-  margin: 0px;
-  padding: 0px;
-  height: 100vh;
-  background-color: green;
-
-  @media screen and (orientation: portrait) {
-    .main {
-      height: 93vh;
-    }
-    .hand {
-      height: 7vh;
-    }
-  }
-
-  @media screen and (orientation: landscape) {
-    .main {
-      height: 90vh;
-    }
-    .hand {
-      height: 10vh;
-    }
-  }
-`;
